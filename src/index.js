8ab150cc999b57292ac10907d3103d4e86781cab
ZumTheZazaKing <zahidi85543@gmail.com>
2021-08-14 23:35:31 +0800

    Firestore added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState,useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import reportWebVitals from './reportWebVitals';
@@ -8,6 +8,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
   apiKey: "AIzaSyDO3KAr7Vw-EyZhlpv2nCpliv2cpotDdkY",
@@ -23,10 +24,59 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function Main(){
+
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+
+  const [messages] = useCollectionData(query, {idField:'id'})
+
+  const [formValue, setFormValue] = useState("");
+
+  const dummy = useRef();
+
+  const sendMessage = async(e) => {
+
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text:formValue,
+      photoUrl:photoURL,
+      uid:uid,
+      createdAt:firebase.firestore.FieldValue.serverTimestamp()
+    })
+
+    setFormValue("");
+
+    dummy.current.scrollIntoView({behavior:'smooth'});
+
+  }
+
   return (<div>
-    <h1>This is main</h1>
+    <main>
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
+      <div ref={dummy}></div>
+    </main>
+
+    <form onSubmit={sendMessage}>
+      <input type="text" value={formValue} onChange={e => setFormValue(e.target.value)}/>
+      <input type="submit" value="Send"/>
+    </form>
     <SignOut/>
   </div>)
+}
+
+function ChatMessage(props){
+
+  const { text, uid, photoUrl } = props.message; 
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return <div className={messageClass}>
+    <img alt="pp" src={photoUrl} width="50" height="50"/>
+    <p>{text}</p>
+  </div>
 }
 
 function SignIn(){
