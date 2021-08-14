e5da7957f940bde568e7785113fb67a1023245a6
ZumTheZazaKing <zahidi85543@gmail.com>
2021-08-14 22:26:53 +0800

    Initial layout of app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './style.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDO3KAr7Vw-EyZhlpv2nCpliv2cpotDdkY",
+  authDomain: "superchat-6b42a.firebaseapp.com",
+  projectId: "superchat-6b42a",
+  storageBucket: "superchat-6b42a.appspot.com",
+  messagingSenderId: "586901058821",
+  appId: "1:586901058821:web:0b122313087c36888ef36c",
+  measurementId: "G-C5BEXQYX3F"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App(){
+  return (<div>
+    <h1>Hello World</h1>
+  </div>)
+}
+
+
+const el = <App/>
+
+ReactDOM.render(el, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
